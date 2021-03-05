94db6a1ff1dade67428518a8131bd5fb24144d91
muaz52 <61652847+muaz52@users.noreply.github.com>
2021-03-05 06:40:36 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("\n{0} eklendi.", customer.CustomerName);
+        }
+
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("\n{0} güncellendi.", customer.CustomerName);
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("\n{0} silindi.", customer.CustomerName);
+        }
+
+        public void GetAllCustomers(Customer[] customersArray)
+        {
+            Console.WriteLine("--------Customer Array--------");
+            foreach(var customer in customersArray)
+            {
+                Console.WriteLine("Customer Name: {0} - Age: {1}",customer.CustomerName,customer.Age);
+            }
+            Console.WriteLine("\n");
+        }
+
+        internal void GetAllCustomers(List<Customer> customersList)
+        {
+            Console.WriteLine("--------Customer List--------");
+            foreach(var customer in customersList)
+            {
+                Console.WriteLine("Customer Name: {0} - Age: {1}", customer.CustomerName, customer.Age);
+            }
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1=new Customer {Id=1,CustomerName="Ahmet",CustomerLastName="Demir",Age=34 };
+            Customer customer2=new Customer {Id=2,CustomerName="Mehmet",CustomerLastName="Öz",Age=65 };
+            Customer customer3=new Customer {Id=3,CustomerName="Ali",CustomerLastName="Saruhan",Age=54 };
+            Customer customer4=new Customer {Id=4,CustomerName="Veli",CustomerLastName="Aydın",Age=23 };
+
+            Customer[] customersArray=new Customer[]{customer1,customer2,customer3 };
+            List<Customer> customersList=new List<Customer>(){customer1,customer2,customer3 };
+            customersList.Add(customer4);
+
+            CustomerManager customerManager= new CustomerManager();
+            customerManager.GetAllCustomers(customersArray);
+            customerManager.GetAllCustomers(customersList);
+
+            customerManager.Add(customer4);
+            customerManager.Update(customer3);
+            customerManager.Delete(customer2);
+
+        }
+    }
+}
